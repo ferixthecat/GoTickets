e7aaf9f66b72ed11bc3d74c426c69a20392202a4
Felix Tse <felixtse283416@gmail.com>
2024-10-01 21:59:23 -0400

    expo setup

---
+++ mobile/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { Slot } from "expo-router";
+
+export default function Root() {
+  return (
+    <>
+      <StatusBar style={"dark"} />
+      {/* Authentication provider */}
+      <Slot />
+      {/* Authentication provider */}
+    </>
+  );
+}
